5738340466a4b0143f513d5a83eb6c57b5074ecb
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2023-02-04 19:40:42 +0100

    get server ip

--- cmd/glinet/main.go
+++ cmd/glinet/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/imroc/req/v3"
+	"github.com/kazhuravlev/just"
 	cli "github.com/urfave/cli/v3"
 	"log"
 	"os"
@@ -12,9 +14,13 @@ import (
 )
 
 const envPassword = "GL_INET_PASSWORD"
-const cfgFile = ".config/glinet/auth-token"
+const cfgFile = ".config/glinet/config.json"
 const argAddr = "addr"
 const argPassword = "password"
+
+type Config struct {
+	Tokens map[string]string `json:"tokens"`
+}
 
 func main() {
 	app := &cli.App{ //nolint:exhaustruct
@@ -38,12 +44,45 @@ func main() {
 					},
 				},
 			},
+			{
+				Name:        "public-ip",
+				Description: "Get public IP addr",
+				Action:      wrapWithClient(cmdGetPublicIP),
+			},
 		},
 	}
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func cmdGetPublicIP(ctx context.Context, c *cli.Context, client *req.Client) error {
+	resp, err := client.R().
+		SetContext(ctx).
+		Get("/cgi-bin/api/internet/public_ip/get")
+	if err != nil {
+		return err
+	}
+
+	var res struct {
+		ServerIP string `json:"serverip"`
+	}
+	if err := resp.UnmarshalJson(&res); err != nil {
+		return err
+	}
+
+	fmt.Println("server IP", res.ServerIP)
+	return nil
+}
+
+func getAbsConfigFile() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(homeDir, cfgFile), nil
 }
 
 func cmdAuth(c *cli.Context) error {
@@ -53,32 +92,82 @@ func cmdAuth(c *cli.Context) error {
 		return errors.New("specify address and password from router")
 	}
 
-	token, err := fetchToken(ctx, c.String(argAddr), c.String(argPassword))
+	glAddr := c.String(argAddr)
+	glToken, err := fetchToken(ctx, glAddr, c.String(argPassword))
 	if err != nil {
 		return err
 	}
 
-	homeDir, err := os.UserHomeDir()
+	absCfgFile, err := getAbsConfigFile()
 	if err != nil {
 		return err
 	}
 
-	absCfgFile := filepath.Join(homeDir, cfgFile)
 	_ = os.MkdirAll(filepath.Dir(absCfgFile), 0o755)
 
-	if err := os.WriteFile(absCfgFile, []byte(token), 0o644); err != nil {
+	cfg := Config{Tokens: make(map[string]string)}
+	if bb, err := os.ReadFile(absCfgFile); err == nil {
+		if err := json.Unmarshal(bb, &cfg); err != nil {
+			return fmt.Errorf("config file is corrupted. check cfg file or delete it: %w", err)
+		}
+	}
+
+	cfg.Tokens[glAddr] = glToken
+	bb, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(absCfgFile, bb, 0o644); err != nil {
 		return err
 	}
 
 	return nil
 }
 
+func wrapWithClient(cmd func(context.Context, *cli.Context, *req.Client) error) cli.ActionFunc {
+	// FIXME: support several routers
+
+	return func(c *cli.Context) error {
+		absConfigFile, err := getAbsConfigFile()
+		if err != nil {
+			return err
+		}
+
+		bb, err := os.ReadFile(absConfigFile)
+		if err != nil {
+			return err
+		}
+
+		var cfg Config
+		if err := json.Unmarshal(bb, &cfg); err != nil {
+			return err
+		}
+
+		if len(cfg.Tokens) != 1 {
+			return errors.New("not implemented")
+		}
+
+		glAddr := just.MapPairs(cfg.Tokens)[0].Key
+		glToken := just.MapPairs(cfg.Tokens)[0].Val
+
+		client := req.C().
+			SetBaseURL(fmt.Sprintf("https://%s", glAddr)).
+			SetCommonHeader("Authorization", glToken).
+			EnableInsecureSkipVerify()
+
+		// TODO: add timeouts
+		ctx := c.Context
+		return cmd(ctx, c, client)
+	}
+}
+
 func fetchToken(ctx context.Context, addr string, password string) (string, error) {
-	client := req.C()
+	client := req.C().EnableInsecureSkipVerify()
 	resp, err := client.R().
 		SetContext(ctx).
 		SetFormData(map[string]string{"pwd": password}).
-		Post(fmt.Sprintf("http://%s/api/router/login", addr))
+		Post(fmt.Sprintf("https://%s/api/router/login", addr))
 	if err != nil {
 		return "", err
 	}
--- go.mod
+++ go.mod
@@ -2,32 +2,41 @@ module github.com/kazhuravlev/glinet-cli
 
 go 1.19
 
-require github.com/urfave/cli/v3 v3.0.0-alpha2
+require (
+	github.com/imroc/req/v3 v3.31.2
+	github.com/kazhuravlev/just v0.57.0
+	github.com/urfave/cli/v3 v3.0.0-alpha2
+)
 
 require (
 	github.com/cheekybits/genny v1.0.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
+	github.com/goccy/go-yaml v1.9.6 // indirect
 	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/imroc/req/v3 v3.31.2 // indirect
 	github.com/marten-seemann/qtls-go1-16 v0.1.5 // indirect
 	github.com/marten-seemann/qtls-go1-17 v0.1.2 // indirect
 	github.com/marten-seemann/qtls-go1-18 v0.1.3 // indirect
 	github.com/marten-seemann/qtls-go1-19 v0.1.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/ginkgo/v2 v2.2.0 // indirect
 	github.com/quic-go/qpack v0.4.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
-	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
-	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/tools v0.1.12 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/exp v0.0.0-20221109134031-9ce248df8de5 // indirect
+	golang.org/x/mod v0.6.0 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/tools v0.2.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
